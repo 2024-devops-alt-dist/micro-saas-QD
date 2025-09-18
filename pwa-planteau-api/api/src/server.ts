6e980f444766c4b45a3275d684fcf4a699e3a7b7
QuentinDegli <quentinnicolasdegli@gmail.com>
2025-09-18 11:51:18 +0200

    feat(api): connexion entre api et la bdd

---
+++ pwa-planteau-api/api/src/server.ts
@@ -0,0 +1,39 @@
+import { Express } from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from "./app";
+import { closeDbConnection, testDbConnection } from './config/db-connection';
+import logger from './middlewares/logger'
+
+const port = process.env.API_PORT || 3000;
+
+const startServer = async () => {
+  try {
+    // Test de la connexion à la base de données avant de démarrer l'API
+    await testDbConnection();
+
+    // Démarrage du serveur
+    app.listen(port, () => {
+      logger.info(`Server is running on http://localhost:${port}`)
+    });
+  } catch (error) {
+    logger.error('Failed to start server: ' + String(error));
+    process.exit(1);
+  }
+};
+
+// Gestion de la fermeture propre du serveur et de la connexion à la BDD
+process.on('SIGINT', async () => {
+    logger.info('SIGINT received: closing server');
+    try {
+      await closeDbConnection();
+      logger.info('DB connection closed');
+    } catch (err) {
+      logger.error('Error closing DB connection: ' + String(err));
+    } finally {
+      process.exit(0);
+    }
+});
+ 
+startServer();
