b0c79de701480c9ccc38eb06b017671f30313942
QuentinDegli <quentinnicolasdegli@gmail.com>
2025-10-15 09:39:23 +0200

    feat(client): add and setup react router

--- pwa-planteau-api/client/src/App.tsx
+++ pwa-planteau-api/client/src/App.tsx
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PlantCollection from './features/plant/pages/PlantCollection';
+import PlantDetail from './features/plant/pages/PlantDetail';
 import Home from './pages/HomePage';
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/plants" element={<PlantCollection />} />
+        <Route path="/plants/:id" element={<PlantDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
