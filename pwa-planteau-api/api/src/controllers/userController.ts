cfce45a818d6014816e788a79be756e93034224b
QuentinDegli <quentinnicolasdegli@gmail.com>
2025-12-18 17:29:59 +0100

    feat(api): add User CRUD

---
+++ pwa-planteau-api/api/src/controllers/userController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import * as service from '../services/userService';
+
+export const getAll = async (_req: Request, res: Response) => {
+  const items = await service.findAll();
+  res.json(items);
+};
+
+export const getById = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const item = await service.findById(id);
+  if (!item) return res.status(404).json({ message: 'Not found' });
+  res.json(item);
+};
+
+export const createOne = async (req: Request, res: Response) => {
+  const created = await service.create(req.body);
+  res.status(201).json(created);
+};
+
+export const deleteOne = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  await service.remove(id);
+  res.status(204).send();
+};
---
+++ pwa-planteau-api/api/src/routes/userRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { getAll, getById, createOne, deleteOne } from '../controllers/userController';
+
+export const router = Router();
+
+router.get('/', getAll);
+router.get('/:id', getById);
+router.post('/', createOne);
+router.delete('/:id', deleteOne);
