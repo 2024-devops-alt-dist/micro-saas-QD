bd7ffe8a7427bd9d5a028b48c95e929168abdf9a
QuentinDegli <quentinnicolasdegli@gmail.com>
2026-02-17 16:52:48 +0100

    feat: affichage checkbox pour marquer une tache effectuée

--- pwa-planteau-api/client/src/features/watering/components/TodayTasks.tsx
+++ pwa-planteau-api/client/src/features/watering/components/TodayTasks.tsx
@@ -21,15 +21,21 @@ const TodayTasks: React.FC<TodayTasksProps> = ({ todayTasks }) => (
     <div className="flex flex-col gap-3">
       {todayTasks.length === 0 && <div className="text-info text-xs">Aucune tâche aujourd'hui</div>}
       {todayTasks.map(task => (
-        <Link
+        <div
           key={task.id_watering}
-          to={`/plants/${task.plantId}`}
-          className="task-cards px-4 py-2 font-semibold shadow flex items-center task-cards-hover"
+          className="task-cards px-4 py-2 font-semibold shadow flex flex-row items-center justify-between task-cards-hover"
         >
-          <span>
+          <Link to={`/plants/${task.plantId}`} className="flex-1 text-left">
             {task.taskLabel} {task.plantName}
-          </span>
-        </Link>
+          </Link>
+          <input
+            type="checkbox"
+            className="ml-3"
+            // checked={false} // à remplacer par la vraie valeur si gestion du statut
+            onChange={() => {}}
+            aria-label="Marquer comme faite"
+          />
+        </div>
       ))}
     </div>
   </div>
